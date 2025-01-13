80f98c8014e4a9f79d4f93628ea3fca861896770
giakiet05 <giakiet05@gmail.com>
2025-01-13 22:31:21 +0700

    add gitignore

--- desktop/SimpleControlDesktop/obj/Debug/net8.0-windows/SimpleControlDesktop.AssemblyInfo.cs
+++ desktop/SimpleControlDesktop/obj/Debug/net8.0-windows/SimpleControlDesktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleControlDesktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15d26e48cb4bb6e08dd02758432d65974b1e1afd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleControlDesktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleControlDesktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
